dff158e757e375c61423de0db48ea8b9345589c9
sho mukaiyama <shomukaiyama3@gmail.com>
2023-10-21 01:07:20 +0900

    fix :app.jsx

--- src/hello_match/resources/views/app.blade.php
+++ src/hello_match/resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <title>Laravel Vite Vue.js 3</title>
-    @viteReactRefresh @vite(['resources/css/app.css', 'resources/js/App.jsx'])
+    @viteReactRefresh @vite(['resources/css/app.css', 'resources/js/app.jsx'])
   </head>
   <body>
     <div id="app"></div>
