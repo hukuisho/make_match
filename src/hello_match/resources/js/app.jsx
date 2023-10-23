dc14694def622f1607c06724b20e5d84ee1a8a3c
sho mukaiyama <shomukaiyama3@gmail.com>
2023-10-23 19:50:35 +0900

    routes

--- src/hello_match/resources/js/app.jsx
+++ src/hello_match/resources/js/app.jsx
@@ -2,13 +2,16 @@ import "./bootstrap";
 // import "./components/Route.jsx";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
+import User from "./components/User/index";
+import CalendarIndex from "./components/calendar/index";
+
+// import { Link } from "react-router-dom";
 
 const App = () => {
     return (
         <>
             <div className="container">
                 <Header />
-                <p>a</p>
             </div>
         </>
     );
---
+++ src/hello_match/resources/js/components/calendar/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+export const CalendarIndex = () => (
+    <>
+        <div>カレンダーのページです</div>
+    </>
+);
+
+export default CalendarIndex;
