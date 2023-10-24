00ca047b878ec5f4890d19e05b37d35d13b324c8
sho mukaiyama <shomukaiyama3@gmail.com>
2023-10-24 21:09:32 +0900

    react-router

---
+++ src/hello_match/resources/js/Routes.js
@@ -0,0 +1,5 @@
+const links = [
+    { to: "/user", text: "User" },
+    { to: "/calendar", text: "Calendar" },
+    // 追加のリンク情報をここに追加する
+];
--- src/hello_match/resources/js/app.jsx
+++ src/hello_match/resources/js/app.jsx
@@ -1,18 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom";
-
+import "./Routes";
 import Header from "./components/Header";
 import Navigation from "./components/navigation";
 import UserIndex from "./components/user/Index";
 import CalendarIndex from "./components/calendar/Index";
 
 const App = () => {
-    const links = [
-        { to: "/user", text: "User" },
-        { to: "/calendar", text: "Calendar" },
-        // 追加のリンク情報をここに追加する
-    ];
-
     return (
         <div className="container">
             <BrowserRouter>
