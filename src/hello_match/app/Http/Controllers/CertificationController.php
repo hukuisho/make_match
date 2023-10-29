a353b8a9ce303690e143450b86b5c727de7440b2
sho mukaiyama <shomukaiyama3@gmail.com>
2023-10-29 17:08:26 +0900

    login

---
+++ src/hello_match/app/Http/Controllers/CertificationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CertificationController extends Controller
+{
+    public function showLoginForm()
+    {
+        // ログインフォームの表示
+    }
+
+    public function login(Request $request)
+    {
+        // ログイン処理
+    }
+
+    public function logout(Request $request)
+    {
+        // ログアウト処理
+    }
+}
